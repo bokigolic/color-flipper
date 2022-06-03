200c280f97979d961bcf1b4d376eaacdb7cc746b
Bojan Golic <bokigolic32@gmail.com>
2022-06-03 15:06:33 -0400

    hex color flopprt works

--- hex.js
+++ hex.js
@@ -5,4 +5,14 @@ const color = document.querySelector('.color');
 
 btn.addEventListener('click', function(){
   let hexColor = '#'
-})
+  for(let i = 0; i < 6; i++){
+    hexColor += hex[getrandomNumber()]
+  }
+
+color.textContent = hexColor;
+document.body.style.backgroundColor = hexColor;
+});
+
+function getrandomNumber(){
+  return Math.floor(Math.random() * hex.length)
+}
